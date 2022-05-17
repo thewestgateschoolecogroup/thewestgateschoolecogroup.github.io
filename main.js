51e3d540545fd200ea09e18261cac8a3183edb85
thewestgateschoolecogroup <105505872+thewestgateschoolecogroup@users.noreply.github.com>
2022-05-17 21:09:15 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function showmenu(){
+    document.getElementById("links").style.zIndex = "10"
+    document.getElementById("links").style.opacity = "1";
+}
+
+function hidemenu(){
+    document.getElementById("links").style.opacity = "0";
+    document.getElementById("links").style.zIndex = "-10"
+}
