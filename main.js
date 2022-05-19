e687c68012bfe9793745a2a7af64bd06402c5e90
thewestgateschoolecogroup <105505872+thewestgateschoolecogroup@users.noreply.github.com>
2022-05-19 07:15:44 +0100

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-function showmenu(){
-    document.getElementById("links").style.zIndex = "10"
-    document.getElementById("links").style.opacity = "1";
-}
-
-function hidemenu(){
-    document.getElementById("links").style.opacity = "0";
-    document.getElementById("links").style.zIndex = "-10"
-}
