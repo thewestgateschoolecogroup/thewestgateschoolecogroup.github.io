66f62b0ecae0090e6300114d3cd62c3f39069bd5
thewestgateschoolecogroup <105505872+thewestgateschoolecogroup@users.noreply.github.com>
2022-07-11 21:46:50 +0000

    update menu

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
+
 function showmenu(){
     document.getElementById("links").style.zIndex = "20000";
-    document.getElementById("links").style.right = "0vw"
+    document.getElementById("links").style.right = "0vw";
+    document.getElementById("links").style.visibility = "visible";
+    document.getElementById("open").style.visibility = "hidden";
+    document.getElementById("close").style.visibility = "visible";
 }
-
 function hidemenu(){
-    document.getElementById("links").style.right = "-100vw"
+    document.getElementById("links").style.right = "-100vw";
     document.getElementById("links").style.zIndex = "-15";
+    document.getElementById("links").style.visibility = "hidden";
+    document.getElementById("hm").style.visibility = "hambrger-menu.png";
+    document.getElementById("open").style.visibility = "visible";
+    document.getElementById("close").style.visibility = "hidden";
 }
-
